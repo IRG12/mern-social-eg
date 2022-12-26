4cc32ada84aa74b9c451f1eebb9a8c677141e544
Isaiah G <isaiahrg12@gmail.com>
2022-12-26 13:15:08 -0600

    typo error in models/Post.js

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* Middle Configuation */
 const __filename = fileURLToPath(import.meta.url);
@@ -59,5 +62,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* Adds dummy data to MongoDB. Comment out "User" and "Post" after you have sent the info to the Back End. Else, you will have duplicate dummy data added everytime you restart the server */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
