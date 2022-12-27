5a40f42a22088996774cf593d7e57e64a4c0887f
Isaiah G <isaiahrg12@gmail.com>
2022-12-27 12:29:06 -0600

    setup 'light' and 'dark' mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,30 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import HomePage from "scences/homePage";
-import LoginPage from "scences/loginPage";
-import ProfilePage from "scences/profilePage";
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+// CssBaseline --> resets CSS to it's noraml CSS
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+  // grabbing "light and dark" mode functionality
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/profile/:userId" element={<ProfilePage />} />
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
