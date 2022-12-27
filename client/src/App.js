0cf6f819ac9f60436e32d9a19ddc16adabca77d6
Isaiah G <isaiahrg12@gmail.com>
2022-12-27 11:56:28 -0600

    setup state with redux and persistState in index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scences/homePage";
+import LoginPage from "scences/loginPage";
+import ProfilePage from "scences/profilePage";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/profile/:userId" element={<ProfilePage />} />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
